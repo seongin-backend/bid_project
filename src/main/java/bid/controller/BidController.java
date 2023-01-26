d260316956196fc8e65b5a7fd78154817f4c4e63
SEONGIN <alterin@naver.com>
2023-01-26 10:40:14 +0900

    RESTAPI 행위에 관여 안하고 자원에만 관여하게 수정

--- src/main/java/bid/controller/BidController.java
+++ src/main/java/bid/controller/BidController.java
@@ -23,62 +23,62 @@ public class BidController {
     public BidController(BidService bidService) {
         this.bidService = bidService;
     }
-    @GetMapping("/bid/master/select")
+    @GetMapping("/bid/master")
     public ResponseEntity<?> selectBidMasterList(@RequestBody BidMasterVo bidMasterVo) {
         List<BidMasterDto> bidMasters = bidService.selectBidMasterList(bidMasterVo);
         return new ResponseEntity<List<BidMasterDto>>(bidMasters, HttpStatus.OK);
     }
-    @PostMapping("/bid/master/insert")
+    @PostMapping("/bid/master")
     public ResponseEntity<?> insertBidMaster(@RequestBody BidMasterVo bidMasterVo) {
         int result = 0;
         result = bidService.insertBidMaster(bidMasterVo);
         if(result < 1) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         return new ResponseEntity<Integer>(result, HttpStatus.OK);
     }
-    @PatchMapping("/bid/master/update")
+    @PatchMapping("/bid/master")
     public ResponseEntity<?> updateBidMaster(@RequestBody BidMasterVo bidMasterVo) {
         int result = 0;
         result = bidService.updateBidMaster(bidMasterVo);
         if(result < 1) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         return new ResponseEntity<Integer>(result, HttpStatus.OK);
     }
-    @DeleteMapping("/bid/master/delete")
+    @DeleteMapping("/bid/master")
     public ResponseEntity<?> deleteBidMaster(@RequestBody BidMasterVo bidMasterVo) {
         int result = 0;
         result = bidService.deleteBidMaster(bidMasterVo);
         if(result < 1) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         return new ResponseEntity<Integer>(result, HttpStatus.OK);
     }
-    @GetMapping("/bid/teukseong/select")
+    @GetMapping("/bid/teukseong")
     public ResponseEntity<?> selectBidTeukseongList(@RequestBody BidTeukseongVo bidTeukseongVo) {
         List<BidTeukseongDto> bidTeukseongVoList = bidService.selectBidTeukseongList(bidTeukseongVo);
         return new ResponseEntity<List<BidTeukseongDto>>(bidTeukseongVoList, HttpStatus.OK);
     }
-    @PostMapping("/bid/teukseong/insert")
+    @PostMapping("/bid/teukseong")
     public ResponseEntity<?> insertBidTeukseong(@RequestBody LinkedList<BidTeukseongVo> bidTeukseongVoList) {
         int result = 0;
         result = bidService.insertBidTeukseong(bidTeukseongVoList);
         if(result < 1) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         return new ResponseEntity<Integer>(result, HttpStatus.OK);
     }
-    @GetMapping("/bid/detail/select")
+    @GetMapping("/bid/detail")
     public ResponseEntity<?> selectBidDetailList(@RequestBody BidDetailVo detailVo) {
         List<BidDetailDto> bidDetailDtos = bidService.selectBidDetailList(detailVo);
         return new ResponseEntity<List<BidDetailDto>>(bidDetailDtos, HttpStatus.OK);
     }
-    @GetMapping("/bid/detail/select/pivot")
+    @GetMapping("/bid/detail/pivot")
     public ResponseEntity<?> selectBidDetailListPivot(@RequestBody BidDetailVo detailVo) throws JsonProcessingException {
         List<BidDetailDtoPivot> bidDetailDtosPivot = bidService.selectBidDetailListPivot(detailVo);
         return new ResponseEntity<List<BidDetailDtoPivot>>(bidDetailDtosPivot, HttpStatus.OK);
     }
-    @PostMapping("/bid/detail/insert")
+    @PostMapping("/bid/detail")
     public ResponseEntity<?> insertBidDetail(@RequestBody LinkedList<BidDetailVo> detailVoList) {
         int result = 0;
         result = bidService.insertBidDetail(detailVoList);
         if(result < 1) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         return new ResponseEntity<Integer>(result, HttpStatus.OK);
     }
-    @DeleteMapping("/bid/detail/delete")
+    @DeleteMapping("/bid/detail")
     public ResponseEntity<?> deleteBidDetail(@RequestBody BidDetailVo detailVo) {
         int result = 0;
         result = bidService.deleteBidDetail(detailVo);
