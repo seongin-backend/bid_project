5ff1a7a101758b53f397dd4913820639c45bbe53
SEONGIN <alterin@naver.com>
2023-01-30 17:05:26 +0900

    log4jdbc로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     implementation("com.oracle.database.jdbc:ojdbc8:21.1.0.0")
     implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.3.0")
     implementation("org.springframework.boot:spring-boot-starter-web:2.7.6")
+    implementation("org.bgee.log4jdbc-log4j2:log4jdbc-log4j2-jdbc4.1:1.16")
     compileOnly("org.projectlombok:lombok:1.18.24")
 }
 
