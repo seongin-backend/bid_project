001958666cdf7561b7eaaef522ef6e225724bd4c
SEONGIN <alterin@naver.com>
2023-02-08 14:06:02 +0900

    입찰 특성 조회 테스트코드 추가

--- src/test/java/bid/controller/BidControllerTest.java
+++ src/test/java/bid/controller/BidControllerTest.java
@@ -19,7 +19,6 @@ import static org.hamcrest.Matchers.is;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BidControllerTest {
 
     @Autowired
@@ -27,9 +26,7 @@ public class BidControllerTest {
 
     @Test
     void 입찰_마스터_조회() throws Exception {
-        ResultActions resultActions;
-        ObjectMapper objectMapper = new ObjectMapper();
-        resultActions = mockMvc.perform(
+        ResultActions resultActions = mockMvc.perform(
                 get("/bid/master")
                         .param("guraeDate", "20230126")
                         .param("baljunkiCompanyCode", "00001")
@@ -48,4 +45,27 @@ public class BidControllerTest {
                 .andExpect(jsonPath("$.baljunkiId", is("BAJUNKI001")));
     }
 
+    @Test
+    void 입찰_특성_조회() throws Exception {
+        String bidId = "20230126_00001_01_BAJUNKI001";
+        ResultActions resultActions = mockMvc.perform(
+                get("/bid/teukseong")
+                        .param("bidId", bidId)
+                        .accept(MediaType.APPLICATION_JSON)
+        );
+        resultActions.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(handler().handlerType(BidRestController.class))
+                .andExpect(handler().methodName("selectBidTeukseongList")) 
+                .andExpect(jsonPath("$[0].bidId", is("20230126_00001_01_BAJUNKI001"))) //pk
+                .andExpect(jsonPath("$[0].teukseongBunryuCode", is("01"))) //pk
+                .andExpect(jsonPath("$[0].teukseongBunryuGubnCode", is("01"))) //pk
+                .andExpect(jsonPath("$[0].teukseongBunryuGubnName", is("열간 기동소요시간")))
+                .andExpect(jsonPath("$[0].submitVal", is("8")))
+                .andExpect(jsonPath("$[0].updateRemk", is("시간 더 걸림")))
+        ;
+    }
+
+
 }
+
